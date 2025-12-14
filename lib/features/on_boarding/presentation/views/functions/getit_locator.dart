9b70677dabc3de087830d97f7e63deb7f5dfed70
amrkory <amrmokamal1@gmail.com>
2025-12-14 13:02:45 +0200

    Remove all old files

--- lib/features/on_boarding/presentation/views/functions/getit_locator.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dalel_app/core/database/cache/cache_helper.dart';
-import 'package:dalel_app/core/services/service_locator.dart';
-
-void OnBoardingVisited(){
-   getIt<CacheHelper>().saveData(key: 'isOnBoardingVisited',value: true);
-}
